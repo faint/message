6b407676f462dd4e96e1136435b6c9c0d56b4170
仲金德 <bunnyfaint@me.com>
2017-02-20 14:22:38 +0800

    change toInt to read and write. add Pack

--- message.go
+++ message.go
@@ -3,12 +3,15 @@ package Server
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 )
 
 // const ...
 const (
-	SizeofType = 4 // Sizeof Message Type
-	SizeofSize = 4 // Sizeof Message Size
+	SizeofType = 4    // Sizeof Message Type
+	SizeofSize = 4    // Sizeof Message Size
+	MaxBuffer  = 1024 // Max Message Content Size
+	SizeofHead = SizeofType + SizeofSize
 )
 
 // Message used for communication between servers and clients
@@ -23,19 +26,41 @@ func Unpack(b []byte) (Message, error) {
 	m := Message{}
 	buf := bytes.NewBuffer(b)
 
-	// read type
-	mType := buf.Next(SizeofType)
-	bufType := bytes.NewBuffer(mType)
-	binary.Read(bufType, binary.LittleEndian, &m.Type)
-	// read size
+	m.Type = read(buf, SizeofType)
+	m.Size = read(buf, SizeofSize)
 
-	// read content
+	if m.Size > MaxBuffer {
+		return m, errors.New("OVER_MAX_BUFFER")
+	}
+
+	mContent := buf.Bytes()
+	rest := int(m.Size - SizeofHead)
+	if rest > 0 {
+		m.Content = mContent[:rest]
+	}
 
 	return m, nil
 }
 
-func toInt(buffer *bytes.Buffer, size int) int {
-	var n int
-	binary.Read(bytes.NewBuffer(buffer.Next(size)), binary.LittleEndian, n)
-	return n
+// Pack message，返回[]byte
+func Pack(mType int, mContent []byte) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	write(buf, int32(mType))
+	sizeofMessage := len(mContent)
+	if sizeofMessage > MaxBuffer {
+		return nil, errors.New("OVER_MAX_BUFFER")
+	}
+	write(buf, int32(SizeofHead+sizeofMessage))
+	write(buf, mContent)
+	return buf.Bytes(), nil
+}
+
+func read(buffer *bytes.Buffer, size int) int {
+	var n int32
+	binary.Read(bytes.NewBuffer(buffer.Next(size)), binary.LittleEndian, &n)
+	return int(n)
+}
+
+func write(buffer *bytes.Buffer, content interface{}) {
+	binary.Write(buffer, binary.LittleEndian, content)
 }
